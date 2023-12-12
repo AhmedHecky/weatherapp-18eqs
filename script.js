c93f32f18c4ef4666ff69ed6bcf385d0851ec302
AhmedHecky <147705662+AhmedHecky@users.noreply.github.com>
2023-12-12 22:27:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,21 @@ function logingoogle()  {
   form.submit()
 }
 
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s;
+  setTimeout(startTime, 1000);
+}
 
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};
+  return i;
+}
 
 let weather = {
     apiKey: "4372882d160efff3d6ada47dd487b31e",
